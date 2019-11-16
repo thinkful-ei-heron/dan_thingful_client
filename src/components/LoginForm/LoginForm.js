32d33e0ef5593dbef12ce521ee1aab9c172856ba
danWagar <dandwagar@gmail.com>
2019-11-16 12:15:13 -0800

    user registration

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Input } from '../Utils/Utils';
-import AuthApiService from '../../services/auth-api-services';
+import AuthApiService from '../../services/auth-api-service';
 import TokenService from '../../services/token-service';
 
 export default class LoginForm extends Component {
